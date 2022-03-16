8023a8ad9bb8f97a9ea04eb83abf2606aecd2370
nursumusod <101688037+nursumusod@users.noreply.github.com>
2022-03-16 22:07:07 +0800

    Update grid-item.js

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,4 +1,4 @@
-import NextLink from'Next/link';
+import NextLink from'next/link';
 import Image from 'next/image'
 import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
